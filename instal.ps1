4c1f956343de7ef87d515fc9fb605b7bace1cecb
anaxita <anaxita.me@gmail.com>
2025-03-15 00:48:29 +0400

    123

--- instal.ps1
+++ instal.ps1
@@ -2,5 +2,5 @@ $User = '.\backup'
 $Password = '@skrUpuF646'
 $SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
-#New-Service -Name 'kopia2' -BinaryPathName '"C:\Users\Gcomp\GolandProjects\kopia\kopia.exe" --config-file="C:\Users\Gcomp\GolandProjects\kopia\repository.config" server start --insecure --htpasswd-file="C:\Users\Gcomp\GolandProjects\kopia\pwd" --server-username=admin@localhost --password=301180 --enable-actions' -DisplayName 'kopia'  -StartupType Automatic
-New-Service -Name 'kopia3' -BinaryPathName '"C:\Users\Gcomp\GolandProjects\kopia\kopia.exe" --config-file="C:\Users\Gcomp\GolandProjects\kopia\repository.config" server start --insecure  --server-username=admin@localhost --password=301180 --enable-actions' -DisplayName 'kopia3'  -StartupType Automatic
+New-Service -Name 'kopia-slava-1' -BinaryPathName '"C:\Program Files\kopia\kopia.exe" --config-file="C:\Program Files\kopia\repository.config" server start --insecure --htpasswd-file="C:\Program Files\kopia\pwd" --server-username=admin@localhost --password=301180 --enable-actions' -DisplayName 'kopia-slava-1' -Credential $Credential -StartupType Automatic
+#New-Service -Name 'kopia5' -BinaryPathName '"C:\Users\Gcomp\GolandProjects\kopia\kopia.exe" server start --insecure --without-password --disable-csrf-token-checks --enable-actions' -DisplayName 'kopia5'  -StartupType Manual
